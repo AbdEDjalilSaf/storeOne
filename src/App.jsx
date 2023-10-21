d5470b5efa530c73a3e8ab90a38dcf3e1ff63514
Abdo saf <noodsaf01@gmail.com>
2023-10-21 23:10:31 +0100

    third store version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import {createStore} from 'redux'
 function App() {
 
 let BUY_Cockes='BUY_Cockes';
@@ -11,11 +11,26 @@ type: BUY_Cockes,
 id:"first redux action"
   }
 }
-let iniales = {
+let inialesState = {
   numOfCakes:10
 }
+let reducer = (state = inialesState,action)=>{
+  switch(action.type){
+  case BUY_Cockes:return{
+    ...state,
+    numOfCakes:state.numOfCakes - 1
+  }
+  default:return state
+  }
+}
+let store = createStore(reducer)
 
+let unsubscribe = store.subscribe(()=>   console.log("update State", store.getState()))
+  // so you are exited
+store.dispatch(cackes())
+store.dispatch(cackes())
 
+unsubscribe();
   return (
     <>
       <p className="read-the-docs">
