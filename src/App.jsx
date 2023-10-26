eec5f2c5a1adc342543b9919d451ae0f62be50c5
Abdo saf <noodsaf01@gmail.com>
2023-10-26 15:19:20 +0100

    sixth store version

--- src/App.jsx
+++ src/App.jsx
@@ -83,123 +83,123 @@ import  Navbar from "./features/postList.jsx"
 
 
 
-function App() {
-
-const Fetch_To_Introdauct = "Fetch_To_Tntrodauct"
-const Fetch_To_Successful = "Fetch_To_Successful"
-const Fetch_To_Filed = "Fetch_To_Filed"
-
-let option_To_Introdauct = ()=>{
-return {
-type:Fetch_To_Introdauct,
-}
-}
-
-let option_To_Successful = (users)=>{
-  return {
-    type:Fetch_To_Successful,
-    pyload:users
-    }
-}
-
-let option_To_Error = (error)=>{
-  return {
-    type:Fetch_To_Filed,
-    pyload:error
-    }
-}
-
-let initialState = {
-  loading:false,
-  users:[],
-  error:''
-}
-
-let reducer = ( state = initialState,action)=>{
-switch(action.type){
-case option_To_Introdauct:
-  return{
-    ...state,
-    loading:true
-  }
-  case option_To_Successful:
-  return{
-    loading:false,
-    users:action.pyload,
-    error:''
-  }
-  case option_To_Error:
-  return{
-    loading:false,
-    users:[],
-    error:action.pyload
-  }
-}
-}
-
-let fitchUser = ()=>{
-  return async function(dispatch){
-    dispatch(option_To_Introdauct())
-    try {
-let response = await axios.get('https://jsonplaceholder.typicode.com/photos');
-let products = await response.data
-products.map((user)=> console.log(user.title))
-dispatch(option_To_Successful);
-console.log(response.data);
-    }catch(err){
-dispatch(option_To_Error(err.message));
-console.log(err.message);
-    }
-  }
-}
-
-let store = createStore(reducer,applyMiddleware(thunkRedux))
-// store.subscribe(()=>{ console.log("state now",store.getState())})
-
-store.dispatch(fitchUser())
-
-return (
-  <>
-<p className="read-the-docs">
-Click on the Vite and React logos to learn more 
-</p>
-<h2 className="read-the-once">
-Click on the Vite and React logos to learn more 
-</h2>
-</>
-)
-}
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // function App() {
-//   <div >
 
-// <h2>Hello Peaple</h2>
-// <Navbar />
-// <h2>Hello Peaple</h2>
-// </div>
+// const Fetch_To_Introdauct = "Fetch_To_Tntrodauct"
+// const Fetch_To_Successful = "Fetch_To_Successful"
+// const Fetch_To_Filed = "Fetch_To_Filed"
 
+// let option_To_Introdauct = ()=>{
+// return {
+// type:Fetch_To_Introdauct,
+// }
+// }
 
+// let option_To_Successful = (users)=>{
+//   return {
+//     type:Fetch_To_Successful,
+//     pyload:users
+//     }
+// }
 
+// let option_To_Error = (error)=>{
+//   return {
+//     type:Fetch_To_Filed,
+//     pyload:error
+//     }
+// }
+
+// let initialState = {
+//   loading:false,
+//   users:[],
+//   error:''
+// }
+
+// let reducer = ( state = initialState,action)=>{
+// switch(action.type){
+// case option_To_Introdauct:
+//   return{
+//     ...state,
+//     loading:true
+//   }
+//   case option_To_Successful:
+//   return{
+//     loading:false,
+//     users:action.pyload,
+//     error:''
+//   }
+//   case option_To_Error:
+//   return{
+//     loading:false,
+//     users:[],
+//     error:action.pyload
+//   }
+// }
+// }
+
+// let fitchUser = ()=>{
+//   return async function(dispatch){
+//     dispatch(option_To_Introdauct())
+//     try {
+// let response = await axios.get('https://jsonplaceholder.typicode.com/photos');
+// let products = await response.data
+// products.map((user)=> console.log(user.title))
+// dispatch(option_To_Successful);
+// console.log(response.data);
+//     }catch(err){
+// dispatch(option_To_Error(err.message));
+// console.log(err.message);
+//     }
+//   }
+// }
+
+// let store = createStore(reducer,applyMiddleware(thunkRedux))
+// // store.subscribe(()=>{ console.log("state now",store.getState())})
+
+// store.dispatch(fitchUser())
+
+// return (
+//   <>
+// <p className="read-the-docs">
+// Click on the Vite and React logos to learn more 
+// </p>
+// <h2 className="read-the-once">
+// Click on the Vite and React logos to learn more 
+// </h2>
+// </>
+// )
 // }
 // export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+  <div className='bg-red-500'>
+
+<h2>Hello Peaple</h2>
+<Navbar />
+<h2>Hello Peaple</h2>
+</div>
+
+
+
+}
+export default App
 
 
 
